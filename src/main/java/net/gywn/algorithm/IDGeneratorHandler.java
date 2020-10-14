a781021b914b97b23ed4126c4d1b399114467949
gywndi <gywndi@gmail.com>
2020-10-14 16:49:00 +0900

    fixed IDGeneratorHandler

--- src/main/java/net/gywn/algorithm/IDGeneratorHandler.java
+++ src/main/java/net/gywn/algorithm/IDGeneratorHandler.java
@@ -1,9 +1,5 @@
 package net.gywn.algorithm;
 
-import java.util.Map;
-
-import net.gywn.common.IDGenerator;
-
 public interface IDGeneratorHandler {
 	public String generate(final String[] params);
 }
