357bed5627ac98b04ff218d098e93f68cbe8d475
gywndi <gywndi@gmail.com>
2021-08-02 14:40:32 +0900

    cache clear

--- src/main/java/net/gywn/algorithm/IDGeneratorHandler.java
+++
@@ -1,5 +0,0 @@
-package net.gywn.algorithm;
-
-public interface IDGeneratorHandler {
-    public String generate(final String[] params);
-}
--- src/main/java/net/gywn/algorithm/RowModifierHandler.java
+++
@@ -1,7 +0,0 @@
-package net.gywn.algorithm;
-
-import java.util.Map;
-
-public interface RowModifierHandler {
-	public void doModify(final Map<String, String> map) throws Exception;
-}
